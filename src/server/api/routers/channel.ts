53b7d8f1fc86c8b223d1633f9e74b3df83902ede
Carson Weeks <mail@carsonweeks.com>
2025-11-13 20:53:37 -0500

    channelpageview

--- src/server/api/routers/channel.ts
+++ src/server/api/routers/channel.ts
@@ -1,5 +1,8 @@
+import { TRPCError } from "@trpc/server";
 import z from "zod";
+import { suiClient } from "~/server/sui";
 import { getAllChannels, getChannelDetails } from "~/services/api";
+import { lookupChannel } from "~/services/backend/channel/lookupChannel";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const channelRouter = createTRPCRouter({
@@ -9,6 +12,20 @@ export const channelRouter = createTRPCRouter({
 			await new Promise((resolve) => setTimeout(resolve, 10));
 			return false;
 		}),
+
+	getChannelByOwner: publicProcedure.input(z.string()).query(async (opts) => {
+		const channel = await lookupChannel(opts.input);
+		if (channel.isErr()) {
+			if (channel.error === "CHANNEL_NOT_FOUND_FOR_ADDRESS") {
+				return null;
+			}
+			throw new TRPCError({
+				code: "BAD_REQUEST",
+				message: channel.error,
+			});
+		}
+		return channel.value;
+	}),
 	list: publicProcedure.query(async () => {
 		return await getAllChannels();
 	}),
