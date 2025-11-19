51c03781984ff6903b9cfc85dd40c7eb319db125
Carson Weeks <mail@carsonweeks.com>
2025-11-19 14:45:02 -0500

    sorted classes

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className="border-t bg-white">
 			<div className="container mx-auto px-4 py-8">
-				<div className=" text-center text-gray-600 text-sm">
+				<div className="text-center text-gray-600 text-sm">
 					<p>&copy; {new Date().getFullYear()} FundSui. All rights reserved.</p>
 				</div>
 			</div>
